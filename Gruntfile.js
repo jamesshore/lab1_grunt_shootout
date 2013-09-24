6d7d503e033676c2e75b5826cdb0bef33a596c38
James Shore <jshore@jamesshore.com>
2013-09-24 16:29:29 -0700

    Got lint working with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,23 @@
+"use strict";
+
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-	  jshint: {
-		  browser: "src/client/**/*.js"
-	  }
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			browser: {
+				options: browserLintOptions(),
+				files: {
+					src: "src/client/**/*.js"
+				}
+			},
+			node: {
+				options: nodeLintOptions(),
+				files: {
+					src: ["src/*.js", "src/server/**/*.js", "build/util/**/*.js", "Gruntfile.js"]
+				}
+			}
+		}
 
 
 //    pkg: grunt.file.readJSON('package.json'),
@@ -17,7 +30,7 @@ module.exports = function(grunt) {
 //        dest: 'build/<%= pkg.name %>.min.js'
 //      }
 //    }
-  });
+	});
 
 //  // Load the plugin that provides the "uglify" task.
 //  grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -30,5 +43,36 @@ module.exports = function(grunt) {
 	grunt.registerTask("default", "Default Description", ["jshint"], function() {
 		console.log("\n\nOK");
 	});
+
+	function globalLintOptions() {
+		return {
+			bitwise: true,
+			curly: false,
+			eqeqeq: true,
+			forin: true,
+			immed: true,
+			latedef: false,
+			newcap: true,
+			noarg: true,
+			noempty: true,
+			nonew: true,
+			regexp: true,
+			undef: true,
+			strict: true,
+			trailing: true
+		};
+	}
+
+	function nodeLintOptions() {
+		var options = globalLintOptions();
+		options.node = true;
+		return options;
+	}
+
+	function browserLintOptions() {
+		var options = globalLintOptions();
+		options.browser = true;
+		return options;
+	}
 
 };
