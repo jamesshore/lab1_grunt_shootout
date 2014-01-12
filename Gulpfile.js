a05bf3c021adecdc2c7fa8f409831d6678ee3f5d
James Shore <jshore@jamesshore.com>
2014-01-12 00:22:30 -0800

    Removed commented code from Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -54,50 +54,6 @@ gulp.task("testClient", function(done) {
 	karma.runTests(REQUIRED_BROWSERS, done, done);
 });
 
-//var lint = require("./build/util/lint_runner.js");
-//var nodeunit = require("./build/util/nodeunit_runner.js");
-//var karma = require("./build/util/karma_runner.js");
-//
-//// Modify (or comment out) the following list to cause the build to fail unless these browsers are tested.
-//var REQUIRED_BROWSERS = [
-//	"IE 8.0.0 (Windows 7)",
-//	"IE 9.0.0 (Windows 7)",
-//	"Firefox 26.0.0 (Mac OS X 10.8)",
-//	"Chrome 31.0.1650 (Mac OS X 10.8.5)",
-//	"Safari 6.1.1 (Mac OS X 10.8.5)",
-//	"Mobile Safari 7.0.0 (iOS 7.0.3)"
-//];
-//
-//desc("Lint and test");
-//task("default", ["lint", "test"], function() {
-//	console.log("\n\nOK");
-//});
-//
-//desc("Start Karma server -- run this first");
-//task("karma", function() {
-//	karma.serve(complete, fail);
-//}, {async: true});
-//
-//desc("Lint everything");
-//task("lint", function () {
-//	var passed = lint.validateFileList(nodeFilesToLint(), nodeLintOptions(), {});
-//	passed = lint.validateFileList(browserFilesToLint(), browserLintOptions(), {}) && passed;
-//	if (!passed) fail("Lint failed");
-//});
-//
-//desc("Test everything");
-//task("test", ["testNode", "testClient"]);
-//
-//desc("Test node.js code");
-//task("testNode", function() {
-//	nodeunit.runTests(nodeFilesToTest(), complete, fail);
-//}, {async: true});
-//
-//desc("Test browser code");
-//task("testClient", function() {
-//	karma.runTests(REQUIRED_BROWSERS, complete, fail);
-//}, {async: true});
-
 function nodeFilesToTest(callback) {
 	glob("src/_*_test.js", function(err, srcFiles) {
 		if (err) return callback(err);
